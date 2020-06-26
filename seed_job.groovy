17d707224fd990bd8b0bc4f9aa2019e7ab0c619e
Anshu Jhalani <anshujhalani98@gmail.com>
2020-06-26 21:35:58 +0530

    Final commit

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,5 +1,6 @@
 job("devops-task6-pull-code"){
     description("This job will pull repo from Github as developer push ")
+    
     scm{
         github('ajtechy/devops-task6-git-kubernetes-jenkins-groovy', 'master')
     }
