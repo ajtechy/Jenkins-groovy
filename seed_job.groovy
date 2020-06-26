be00742db5c1e773e1ddc9efed0edfbfc19ffddc
Anshu Jhalani <anshujhalani98@gmail.com>
2020-06-26 16:00:53 +0530

    first commit

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,7 +1,7 @@
 job("devops-task6-pull-code"){
     description("This job will pull repo from Github as developer push ")
     scm{
-        github('ajtechy/devops-task6-jenkins-groovy', 'master')
+        github('ajtechy/devops-task6-git-kubernetes-jenkins-groovy', 'master')
     }
     triggers{
         githubPush()
@@ -10,3 +10,56 @@ job("devops-task6-pull-code"){
     preBuildCleanup()
   }
 }
+
+job("devops-task6-check-code"){
+    description("This job will check code and deploy environment accordingly")
+  
+    triggers {
+        upstream("devops-task6-pull-code")
+    }
+
+    steps {
+        shell(readFileFromWorkspace("check-code.sh"))
+    
+}
+
+job("devops-task6-test-code") {
+    description("This job will test whether our webserver is working or not and also tigger to notify job")
+  
+    triggers {
+         upstream("devops-task6-check-code")
+    }
+
+    steps {
+        shell(readFileFromWorkspace("test-webserver.sh"))
+    }
+}
+
+job("devops-task6-notify-mail") {
+    description("This job will  Send Mail to Developer if webserver is not working properly ")
+  
+    triggers {
+        upstream("devops-task6-test-code")
+    }
+    
+    publishers {
+        extendedEmail {
+        contentType('text/html')
+        triggers {
+            success{
+                attachBuildLog(true)
+                subject('Build successfull')
+                content('The build was successful and deployment was done.')
+                recipientList('anshujhalani98@gmail.com')
+            }
+            failure{
+                attachBuildLog(true)
+                subject('Failed build')
+                content('The build was failed')
+                recipientList('anshujhalani98@gmail.com')
+            }
+        }
+        }
+    }
+}
+
---
+++ test-webserver.sh
@@ -0,0 +1,38 @@
+for file in $(ls /root/devops-task3/devops-task3-pull-code/)
+do
+	if [[ -n $(echo $file | grep html) ]]
+    then
+    	export NodePort=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
+        echo $NodePort
+        export status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.101:$NodePort/$file)
+        echo $status_code
+        echo $file $NodePort $status_code
+        if [[ $status_code == 200 ]]
+        then
+        	echo "Everything is alright.\nwebserver is running good"
+            exit 1
+        else
+        	echo "Something is wrong"
+            # if something is wrong then this job fill notify to developer via mail
+            # and also trigger to notify mail job.
+            exit 0
+    	fi
+    elif [[ -n $(echo $file | grep php) ]]
+    then
+    	export NodePort=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[*].nodePort}')
+        echo $NodePort
+        export status_code=$(curl -s -o /dev/null -w "%{http_code}" 192.168.99.101:$NodePort/$file)
+        echo $status_code
+        echo $file $NodePort $status_code
+        if [[ $status_code == 200 ]]
+        then
+        	echo "Everything is alright.\nwebserver is running good"
+            exit 1
+        else
+        	echo "Something is wrong"
+            # if something is wrong then this job fill notify to developer via mail
+            # and also trigger to notify mail job.
+            exit 0
+        fi
+    fi
+done
